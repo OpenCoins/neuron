38dc7333c9d08375ed2b9ee7ce7c11b8ba25647d
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2024-01-09 22:34:48 +0800

    Update prepare_neuron_windows.bat

--- packages/sync-test/scripts/prepare_neuron_windows.bat
+++ packages/sync-test/scripts/prepare_neuron_windows.bat
@@ -1,6 +1,6 @@
 curl -O -L  http://github-test-logs.ckbapp.dev/neuron/sync/Neuron-v0.112.0-setup.exe
 move .\Neuron-*.exe Neuron-setup.exe
-.\Neuron-setup.exe /S /D=C:\Users\linguopeng_112963420\IdeaProjects\neuron\packages\sync-test\neuron
+.\Neuron-setup.exe /S /D=D:\a\neuron\neuron\packages\sync-test\neuron
 mkdir ".\source\bin"
 copy ".\neuron\bin\ckb.exe" ".\source\bin\ckb.exe"
 copy ".\neuron\bin\ckb-light-client.exe" ".\source\bin\ckb-light-client.exe"
