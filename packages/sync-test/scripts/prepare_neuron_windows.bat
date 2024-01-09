b6efcb0d43d66655a00c6fa009ce8e5a15f4c7cd
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2024-01-09 15:57:34 +0800

    Update prepare_neuron_windows.bat

--- packages/sync-test/scripts/prepare_neuron_windows.bat
+++ packages/sync-test/scripts/prepare_neuron_windows.bat
@@ -1,10 +1,17 @@
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM set time out
+SET TIMEOUT_SECONDS=300
+SET WAIT_SECONDS=5
+SET ELAPSED_SECONDS=0
 curl -O -L  http://github-test-logs.ckbapp.dev/neuron/sync/Neuron-v0.112.0-setup.exe
+
 copy .\Neuron-*.exe Neuron-setup.exe
 @echo off
-REM 执行安装程序
+REM install 
 .\Neuron-setup.exe /S /D=D:\a\neuron\neuron\packages\sync-test\neuron
 
-REM 等待进程执行结束
+REM wait install successful
 :CHECK_LOOP
 tasklist | find "Neuron-setup.exe" > nul
 if errorlevel 1 (
@@ -14,6 +21,12 @@ if errorlevel 1 (
     copy ".\neuron\bin\ckb-light-client.exe" ".\source\bin\ckb-light-client.exe"
     exit /b 0
 ) else (
-    timeout /t 5 /nobreak > nul
-    goto :CHECK_LOOP
+		if !ELAPSED_SECONDS! GEQ %TIMEOUT_SECONDS% (
+						echo Error: Timeout occurred while waiting for process to finish.
+						exit /b 1
+				) else (
+						timeout /t %WAIT_SECONDS% /nobreak > nul
+						SET /A ELAPSED_SECONDS+=WAIT_SECONDS
+						goto :CHECK_LOOP
+		)
 )
