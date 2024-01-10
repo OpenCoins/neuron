c1fb8f4d1fe0303b24efde2dbbc5d3f3f35795c6
gpBlockchain <744158715@qq.com>
2024-01-10 10:48:03 +0800

    test ckb 113

--- packages/sync-test/scripts/prepare_neuron_macos.sh
+++ packages/sync-test/scripts/prepare_neuron_macos.sh
@@ -4,3 +4,6 @@ unzip Neuron.zip
 mv Neuron.app neuron
 chmod 777 neuron/Contents
 cp -r neuron/Contents/bin source/
+wget https://github.com/nervosnetwork/ckb/releases/download/v0.113.0-rc3/ckb_v0.113.0-rc3_x86_64-apple-darwin-portable.zip
+unzip ckb_v0.113.0-rc3_x86_64-apple-darwin-portable.zip
+cp ckb_v0.113.0-rc3_x86_64-apple-darwin-portable/ckb source/bin/ckb
